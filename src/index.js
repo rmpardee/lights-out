4a1499a9963147a6a2c39df8e99604d3d8b4b1c6
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-12-05 19:22:35 -0800

    Clicking a square toggles its immediate neighbors successfully.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './board.js';
+
+ReactDOM.render(
+  <Board size='8'/>
+, document.querySelector('#container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: [
-    './script.js'
+    './src/index.js'
   ],
   output: {
     path: __dirname,
